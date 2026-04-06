6f262ea4f926529c37784c537270f3011d6b7c15
Nhu Cong Viet Hoang <hoangncv@thienhoang.com.vn>
2026-04-06 10:14:30 +0700

    Configure Firebase project for shared cloud sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCZeIE9bAyZ-17x2n29Ba8QUomJXeKDYAM",
+  authDomain: "test01-56144.firebaseapp.com",
+  projectId: "test01-56144",
+  storageBucket: "test01-56144.firebasestorage.app",
+  messagingSenderId: "456358401002",
+  appId: "1:456358401002:web:5ab3cf162af86dc0649893"
 };
 
 // No-login cloud sync note:
